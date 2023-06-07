9ad31bdce5cc5af0521566e226693393623d981f
koobai <koobai@users.noreply.github.com>
2023-06-07 14:23:51 +0000

    deploy: koobai/blog@9de6e46484c309c2751de775161fc0fa5e84d4a7

--- js/about-photo.js
+++ js/about-photo.js
@@ -24,7 +24,7 @@ const insertRandomImages = images => {
   });
 };
 
-fetch('/js/photo.json')
+fetch('/suju/photo.json')
   .then(response => response.json())
   .then(data => {
     insertRandomImages(getRandomImages(data, 12));
